47fcca647ebdf328dbc9837e9823e65ddb955cff
jcstange <jcstange@gmail.com>
2021-08-26 23:46:04 +0300

    Add jest

--- src/__tests__/components/job.tsx
+++ src/__tests__/components/job.tsx
@@ -2,5 +2,9 @@ import React from 'react'
 import { JobComponent } from '../../components/job'
 
 test('Useless test', () => {
-    expect(1+2).toBe(4)
+    expect(1+2).toBe(3)
+})
+
+test('test JobComponent somehow', () => {
+    expect(1+2).toBe(3)
 })
